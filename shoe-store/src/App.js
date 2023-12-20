931364f3b4716421b69528edb028f20d6a883766
Brandon <rbrandon087@gmail.com>
2023-12-19 22:46:35 -0800

    setup my component and pages

---
+++ shoe-store/src/App.js
@@ -0,0 +1,11 @@
+import Navbar  from './Component/Navbar';
+import Home from './Pages/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div>Hi</div>
+  );
+}
+
+export default App;
---
+++ shoe-store/src/Component/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div>cunt</div>
+    )
+ }
+
+ export default Navbar;
